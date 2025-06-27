cedad0b03a60a050c4237d33ba2f8a52ff86a21c
pranav <pranavamaragani503@gmail.com>
2025-06-27 19:12:16 +0530

    final code

--- server.js
+++ server.js
@@ -8,10 +8,6 @@ const { requestRouter } = require("./routers/requests.js");
 const { userRouter } = require("./routers/user.js");
 const cors = require("cors");
 
-
-
-
-
 app.use(express.json());
 app.use(cookieParser());
 
@@ -20,12 +16,8 @@ app.use(
     origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
-    
   })
 );
-
-
-
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
